8745c39c17e596feb5ce25038e1b6abb197aca5b
nukantcoder <kukrejanukant@gmail.com>
2025-09-27 14:13:19 +0530

    Time: 0 ms (100%), Space: 95.2 MB (48.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,80 +1,57 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        int n1 = m + n;
-        int mid1 = (n1/2) - 1;
-        int mid2 = mid1 + 1;
-        int element1;
-        int element2;
-        int i = 0;
-        int j = 0;
-        int count = -1;
-        while(i < m && j < n)
+        
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1 > n2)
+        return findMedianSortedArrays(nums2,nums1);
+
+        int low = 0;
+        int high = n1;
+        double ans1,ans2;
+        while(low <= high)
+        {
+            int mid1 = (low + high)/2;
+            int elements = (n1 + n2 + 1) / 2;
+            int mid2 = elements - mid1;
+            int l1 = INT_MIN,l2 = INT_MIN;
+            int r1 = INT_MAX,r2 = INT_MAX;
+
+            if(mid1 < n1)
+            r1 = nums1[mid1];
+            if(mid2 < n2)
+            r2 = nums2[mid2];
+            if(mid1 - 1 >= 0)
+            l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0)
+            l2 = nums2[mid2 - 1];
+
+            if(l1 <= r2 && l2 <= r1)
             {
-                if(nums1[i] <= nums2[j])
+                if((n1 + n2) % 2 == 0)
                 {
-                    count++;
-                    if(count == mid1)
-                    {
-                        element1 = nums1[i];
-                    }
-                    if(count == mid2)
-                    {
-                        element2 = nums1[i];
-                    }
-                    i++;
+                double ans1 = (double(max(l1,l2) + min(r1,r2)))/2.0;
+                return ans1;
                 }
                 else
                 {
-                                        count++;
-                    if(count == mid1)
-                    {
-                        element1 = nums2[j];
-                    }
-                    if(count == mid2)
-                    {
-                        element2 = nums2[j];
-                    }
-                    j++;
+                double ans2 = double(max(l1,l2));
+                return ans2;
                 }
             }
-        while( i < m)
+            else if(l1 > r2)
             {
-                    count++;
-                    if(count == mid1)
-                    {
-                        element1 = nums1[i];
-                    }
-                    if(count == mid2)
-                    {
-                        element2 = nums1[i];
-                    }
-                    i++;
+                high = mid1 - 1;
             }
-        while(j < n)
+            else
             {
-                                    count++;
-                    if(count == mid1)
-                    {
-                        element1 = nums2[j];
-                    }
-                    if(count == mid2)
-                    {
-                        element2 = nums2[j];
-                    }
-                    j++;
+                if(l2 > r1)
+                {
+                    low = mid1 + 1;
+                }
             }
-        if(n1 % 2 == 0)
-        {
-            return double((double(element1) + double(element2))/2.0);
         }
-        else
-        {
-            return double(element2);
-        }
-        
-        
+        return 0.0;
     }
 };
