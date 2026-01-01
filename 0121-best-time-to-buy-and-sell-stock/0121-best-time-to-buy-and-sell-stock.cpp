c42b0eb6967c420d56057f679a7b71e4850d571b
nukantcoder <kukrejanukant@gmail.com>
2026-01-01 23:16:47 +0530

    Time: 0 ms (100%), Space: 97.4 MB (24.88%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        
         int n = prices.size();
-        int minvalue = prices[0];
-        int maxprofit = 0;
-        for(int i = 0;i < n;i++)
-            {
-                minvalue = min(minvalue,prices[i]);
 
-                maxprofit = max(maxprofit,prices[i] - minvalue);
-            }
+        //Brute Force Approach is find all the profit possible using nested loops
+        //Optimal Approach i thought was we can track the minimum alongwith maxprofit and updating both while traversing the array
+
+        int maxprofit = 0;
+        int currentmin = prices[0];
+        for(int i = 1;i < n;i++)
+        {
+            maxprofit = max(maxprofit,prices[i] - currentmin);
+
+           currentmin = min(currentmin,prices[i]);
+        }
         return maxprofit;
+
+        //atlast if it works i am surprised because the logic clicked very quickly because i had done it earlier 
     }
 };
