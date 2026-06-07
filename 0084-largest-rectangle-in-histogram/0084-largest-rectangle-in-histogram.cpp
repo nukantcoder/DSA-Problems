76f78401f36e50d1ef022d5337e025b8cdf3c018
nukantcoder <kukrejanukant@gmail.com>
2026-06-07 14:43:10 +0530

    Time: 19 ms (70.78%), Space: 81.4 MB (70.69%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,63 +1,49 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int>st;
-        int maxarea = 0;
-
         int n = heights.size();
-        vector<int>NSE(n,0);
-        vector<int>PSE(n,0);
-
-        //Next Smaller element
-        NSE[n - 1] = n;
-        st.push(n - 1);
-        for(int i = n - 2;i >= 0;i--)
+        stack<int>st;
+        int current = 0;
+        int nse = 0;
+        int pse = 0;
+        int area = 0;
+        st.push(0);
+        for(int i = 1;i < n;i++)
         {
-            while(!st.empty() && heights[st.top()] >= heights[i])
+            while(!st.empty() && heights[st.top()] > heights[i])
             {
+                current = st.top();
                 st.pop();
-            }
-            if(!st.empty() && heights[st.top()] < heights[i])
-            {
-                NSE[i] = st.top();
-            }
-            if(st.empty())
-            {
-                NSE[i] = n;
+                nse = i;
+                if(!st.empty())
+                {
+                    pse = st.top();
+                }
+                else
+                {
+                    pse = -1;
+                }
+                area = max(area,heights[current]*(nse - pse - 1));
             }
             st.push(i);
         }
 
         while(st.size() != 0)
         {
-            st.pop();
-        }
-
-        //previous smaller element
-
-        PSE[0] = -1;
-        st.push(0);
-        for(int i = 1;i < n;i++)
-        {
-             while(!st.empty() && heights[st.top()] >= heights[i])
-            {
+                current = st.top();
                 st.pop();
-            }
-            if(!st.empty() && heights[st.top()] < heights[i])
-            {
-                PSE[i] = st.top();
-            }
-            if(st.empty())
-            {
-                PSE[i] = -1;
-            }
-            st.push(i);
+                nse = n; //here since no elements are left but elemets still remain in stack so for them no next smaller element exisits so we will take as n
+                if(!st.empty())
+                {
+                    pse = st.top();
+                }
+                else
+                {
+                    pse = -1;
+                }
+                area = max(area,heights[current]*(nse - pse - 1));
         }
 
-        for(int i = 0;i < n;i++)
-        {
-            maxarea = max(maxarea,heights[i] *(NSE[i] - PSE[i] - 1));
-        }
-        return maxarea;
+        return area;
     }
 };
