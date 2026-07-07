1bebf944dc1107b3a9e792d567e2734e741aeeb8
nukantcoder <kukrejanukant@gmail.com>
2026-07-07 15:39:38 +0530

    Time: 0 ms (100%), Space: 11.1 MB (51.78%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
+int minval(int a,int b,int c)
+{
+    if(a <= b && a <= c) return a;
+    else if(b <= a && b <= c) return b;
+    else return c;
+}
     int numberOfSubstrings(string s) {
-        //The main idea behind the optimal approach is instaed of finding all the subarrays expand to find the minimum valid window from index i and then count the valid subarrays using n - right then shrink the window because we have counted the number of subarrays from current i and so on 
-        //Baisc idea is expand right to find minimal valid substring 
+        //The main idea behind this approach is to count how many substrings can end at this particular index in order to tell how many substrings are valid at this particular index just see the minimum vlaue between lasta lastb and last c since minimum value indicates this is valid substring and to left to it are all valid substrings
 
         int n = s.size();
+        int lastA = -1;
+        int lastB = -1;
+        int lastC = -1;
         int ans = 0;
-        unordered_map<char,int>mp;
 
-        int i = 0;
-        int j = 0;
-        while(j < n)
+        for(int i = 0;i < n;i++)
         {
-            mp[s[j]]++;
-            while(mp.size() == 3)
+            if(s[i] == 'a') lastA = i;
+            if(s[i] == 'b') lastB = i;
+            if(s[i] == 'c') lastC = i;
+
+            if(lastA >= 0 && lastB >= 0 && lastC >= 0)
             {
-                ans = ans + (n - j);
-                mp[s[i]]--;
-                if(mp[s[i]] == 0)
-                {
-                    mp.erase(s[i]);
-                }
-                i++;
+                int val = minval(lastA,lastB,lastC);
+                ans = ans + val + 1;
             }
-            j++;
         }
         return ans;
     }
