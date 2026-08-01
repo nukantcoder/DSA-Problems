60d083bae0dc77c7bdaeae6f1b09d2d302bbaf1a
nukantcoder <kukrejanukant@gmail.com>
2026-08-01 15:40:39 +0530

    Time: 0 ms (100%), Space: 23 MB (56.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,34 +11,37 @@
  */
 class Solution {
 public:
-int height(TreeNode* root)
+//The main approach behind the optimized solution is in brute force approach approach we were calculating the height again and again for a node so it was taking O(N^2) time now instead of separately calculating balnced and height when we will have a nodes left subtree height and right subtree height will will calculate itself and if   if(abs(left - right) > 1) return - 1 which will indicate this node is balanced and will return -1 to upward unfinished call and left == -1 and right == -1 will make sure early termination indicating an unbalanced subtree. Time Complexity:O(N) Auxilary Space:O(N) in worst case stack space 
+int checkheight(TreeNode* root)
 {
     if(root == NULL)
     {
         return 0;
     }
-    int left = height(root -> left);
-    int right = height(root -> right);
+    int left = checkheight(root -> left);
+    if(left == -1)
+    {
+        return -1;
+    }
+    int right = checkheight(root -> right);
+    if(right == -1)
+    {
+        return -1;
+    }
 
+    if(abs(left - right) > 1)
+    {
+        return -1;
+    }
     return 1 + max(left,right);
 }
     bool isBalanced(TreeNode* root) {
-        if(root == NULL)
+        
+        int val = checkheight(root);
+        if(val != -1)
         {
             return true;
         }
-        int left = height(root -> left);
-        int right = height(root -> right);
-
-        if(abs(left - right) > 1) return false;
-
-        bool leftsubtree = isBalanced(root -> left);
-        bool rightsubtree = isBalanced(root -> right);
-         
-         if(leftsubtree == false || rightsubtree == false)
-         {
-            return false;
-         }
-         return true;
+        return false;
     }
 };
