7a8f4a180e6705b7e4ae6aef91f48a404ad5a6d7
nukantcoder <kukrejanukant@gmail.com>
2026-08-03 19:27:09 +0530

    Time: 0 ms (100%), Space: 23.6 MB (73.4%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,28 +11,23 @@
  */
 class Solution {
 public:
-//Brute Force Approach - for every node were calculating its throughroot path, its leftpath and its rightpath now i understood this approach   int leftdiameter = diameter(root -> left);
-   //     int rightdiameter = diameter(root -> right); why these are written as they were gib=ving the longest diameter from left and right respeactively Time complexity:O(n) for every node * O(n) for every node we are going till leaf so all the way down O(n^2)
-int height(TreeNode* root)
-{
-    if(root == NULL)
+int diameter = 0;
+    int height(TreeNode* root)
     {
-        return 0;
-    }
-    int left = height(root -> left);
-    int right = height(root -> right);
-    return 1 + max(left,right);
-}
-    int diameterOfBinaryTree(TreeNode* root) {
         if(root == NULL)
         {
             return 0;
         }
-        int leftheight = height(root -> left);// since these both are being calculated again and again these must be optimized 
-        int rightheight = height(root -> right);//
-        int troot = leftheight + rightheight;
-        int leftdiameter = diameterOfBinaryTree(root -> left);
-        int rightdiameter = diameterOfBinaryTree(root -> right);
-        return max(troot,max(leftdiameter,rightdiameter));
+        int left = height(root -> left);
+        int right = height(root -> right);
+        int throughroot = left + right;
+
+        diameter = max(diameter,throughroot);
+        return 1 + max(left,right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+     //The main approach behind optimal solution is instead of finding height again and again for several nodes just compute the leftheight and rightheight of a node and leftheight + rightheight update diameter and return height TC:O(N) AS:O(N) recursion stack space worst case 
+     int val = height(root);
+     return diameter;
     }
 };
